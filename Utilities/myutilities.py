41e7a15f167c075f846d882a59e20834ccc22d49
gol3tron <adamgoler@gmail.com>
2021-06-24 09:34:30 -0700

    added utilities dir

---
+++ Utilities/myutilities.py
@@ -0,0 +1,5 @@
+def str2bin(input):
+	# converts a string of text to a binary string
+	# ex: input = 'test'; output = '1110100110010111100111110100'
+	''.join(format(ord(i),'b') for i in x)
+	return binary_out_string
